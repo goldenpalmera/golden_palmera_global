578321b87b384544daf0b37b91fe3d40b44a016b
unknown <ferdinandcharles6@gmail.com>
2026-09-03 23:07:10 +0100

    Solved CICD docker google auth issues

--- auth.ts
+++ auth.ts
@@ -1,28 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-function getAdminEmail(): string {
-  const adminEmail =
-    process.env.ADMIN_EMAIL?.toLowerCase();
-
-  if (!adminEmail) {
-    throw new Error(
-      "ADMIN_EMAIL is not configured."
-    );
-  }
-  return adminEmail;
-}
-
-const adminEmail = getAdminEmail();
-
-const googleClientId = process.env.AUTH_GOOGLE_ID!;
-const googleClientSecret = process.env.AUTH_GOOGLE_SECRET!;
-
-if (!googleClientId || !googleClientSecret) {
-  throw new Error("Google OAuth credentials are not configured.");
-}
-
-
 export const {
   handlers,
   auth,
@@ -30,10 +8,7 @@ export const {
   signOut,
 } = NextAuth({
   providers: [
-    Google({
-      clientId: googleClientId,
-      clientSecret: googleClientSecret
-    }),
+    Google,
   ],
 
   session: {
@@ -46,9 +21,10 @@ export const {
 
   callbacks: {
     async signIn({ user }) {
+      const adminEmail = process.env.ADMIN_EMAIL?.toLowerCase();
       const email = user.email?.toLowerCase();
 
-      if (!email) {
+      if (!adminEmail || !email) {
         return false;
       }
 
